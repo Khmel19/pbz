72ec4381d4983c691c8da97a434f0a64106d85ea
Khmel19 <khmialinka@gmail.com>
2020-10-06 01:24:08 +0300

    updated TeacherDao update()

--- trainingOnCourses/src/main/java/com/khmel/dao/TeacherDao.java
+++ trainingOnCourses/src/main/java/com/khmel/dao/TeacherDao.java
@@ -8,8 +8,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
-public class TeacherDao implements GenericDao<Teacher>{
-private Connection connection;
+public class TeacherDao implements GenericDao<Teacher> {
+    private Connection connection;
 
     public TeacherDao() {
         this.connection = DB.getConnection();
@@ -17,9 +17,9 @@ private Connection connection;
 
     @Override
     public void create(Teacher teacher) throws SQLException {
-        PreparedStatement preparedStatement=connection.prepareStatement("INSERT INTO  teacher VALUES(?,?,?,?)");
-        preparedStatement.setString(1,teacher.getName());
-        
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO  teacher VALUES(?,?,?,?)");
+        preparedStatement.setString(1, teacher.getName());
+
     }
 
     @Override
@@ -28,7 +28,26 @@ private Connection connection;
     }
 
     @Override
-    public int update(Teacher object) {
+    public int update(Teacher teacher) {
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement("UPDATE " +
+                    teacher.getClass().getSimpleName().toLowerCase() + ""
+                    + " SET name=?, birthday=?," +
+                    " education=?, category=? WHERE id=" + teacher.getId());
+            try {
+                ps.setString(4, teacher.getCategory());
+                ps.setString(1, teacher.getName());
+                ps.setString(2, teacher.getBirthday());
+                ps.setString(3, teacher.getEducation());
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            ps.execute();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
         return 0;
     }
 
